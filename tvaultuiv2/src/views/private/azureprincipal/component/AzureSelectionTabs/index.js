b31c8b2ed616b58511bef4749540a7bf18bed5ec
MikeMike41 <Michael.Livingston10@T-Mobile.com>
2022-03-27 21:49:03 -0500

    removing unnecessary change

--- tvaultuiv2/src/views/private/azureprincipal/component/AzureSelectionTabs/index.js
+++ tvaultuiv2/src/views/private/azureprincipal/component/AzureSelectionTabs/index.js
@@ -121,11 +121,8 @@ const AzureSelectionTabs = (props) => {
       .getAzureserviceDetails(`${azureDetail.name}`)
       .then((res) => {
         setAzureMetaData(res.data);
-        if (
-          res?.data?.isActivated &&
-          (res?.data?.owner_ntid?.toLowerCase() ===
-            sessionStorage.getItem('username').toLowerCase() ||
-            isAdmin)
+        if (res?.data?.isActivated && ((res?.data?.owner_ntid?.toLowerCase() ===
+        sessionStorage.getItem('username').toLowerCase()) || isAdmin)
         ) {
           setHasPermission(true);
           getEachUser(res.data.users);
